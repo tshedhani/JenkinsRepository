c6cdcd225fa8a518a996d61054c7ca1d23f8a56d
Tarun <tshedhani@gmail.com>
2018-10-18 01:17:16 -0400

    second commit

--- src/main/java/com/example/JenkinsExampleApplication.java
+++ src/main/java/com/example/JenkinsExampleApplication.java
@@ -1,12 +1,19 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JenkinsExampleApplication {
+public class JenkinsExampleApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsExampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 }
